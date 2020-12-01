470b01eaf667a884b538a32214e2c3a0b9835b26
sembb <sembarelds37@gmail.com>
2020-12-01 17:43:19 +0100

    Comments neergezet en een aantal kleine wijzigingen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,52 +10,52 @@ import { WeerService } from '../services/weer.service';
 
 export class HomePage implements OnInit {
 
-  weerdata;
+  weerdata = Array;
   city;
-  algemeneOmschrijving;
-  temp;
-  weerIcon;
   metString;
-  weerBackground;
 
+
+  //Inject the weerservice so i can use it here.
   constructor(private WeerService: WeerService) {}
 
   ngOnInit(){
-    this.city = this.WeerService.city;
-    this.WeerService.searchData(this.city).subscribe(data =>{
-      this.temp = JSON.stringify(data['main']['temp']);
-      this.algemeneOmschrijving = JSON.stringify(data['weather'][0]['description']);
-      this.metString = JSON.stringify(data['weather'][0]['icon']).replace(/\"/g, "");
-      this.weerIcon = "http://openweathermap.org/img/wn/" + this.metString + "@2x.png";
-      this.weerBackground = JSON.stringify(data['weather'][0]['main']).replace(/\"/g, "");
-      if(this.weerBackground == 'Haze' || this.weerBackground == 'Smoke' || this.weerBackground == 'Dust' || this.weerBackground == 'Fog' || this.weerBackground == 'Sand' || this.weerBackground == 'Ash'){
-        this.weerBackground = 'Mist';
-      }
-      this.weerBackground = "assets/images/" + this.weerBackground.toLowerCase() + '.jpg'; 
-      this.weerdata = this.temp;
+      
+    //On initialise get weather info and put the info in variables.
+      this.city = this.WeerService.city;
+      this.WeerService.searchData(this.city).subscribe(data =>{
+        this.initialiseVars(data);
     });
   }
 
   withSearch(searchValue: string){
+    //onsearch refresh the search and change the city to the input.
     if(searchValue == ''){
       this.city = 'Amsterdam';
     }else{
     this.city = searchValue;
     }
+    
     this.WeerService.searchData(searchValue).subscribe( data =>{
-      this.temp = JSON.stringify(data['main']['temp']);
-      this.algemeneOmschrijving = JSON.stringify(data['weather'][0]['description']);
-      this.metString = JSON.stringify(data['weather'][0]['icon']).replace(/\"/g, "");
-      this.weerIcon = "http://openweathermap.org/img/wn/" + this.metString + "@2x.png";
-      this.weerdata = this.temp;
-      this.weerBackground = JSON.stringify(data['weather'][0]['main']).replace(/\"/g, "");
-      if(this.weerBackground == 'Haze' || this.weerBackground == 'Smoke' || this.weerBackground == 'Dust' || this.weerBackground == 'Fog' || this.weerBackground == 'Sand' || this.weerBackground == 'Ash'){
-        this.weerBackground = 'Mist';
-      }
-      this.weerBackground = "assets/images/" + this.weerBackground.toLowerCase() + '.jpg'; 
+      this.initialiseVars(data);
+
+      //if city is not found, give an error.
     }, err => {
-      this.weerdata = "Er is geen resultaat gevonden voor " + this.city;
+      this.weerdata['temp'] = "Er is geen resultaat gevonden";
+      this.weerdata['algemeneOmschrijving'] = '';
     });
+  }
+
+  initialiseVars(data){
+    //defines all the variables the program needs to display the weather info.
+    this.weerdata['temp'] = 'Het is ' + JSON.stringify(data['main']['temp'])+'°C in '+ this.city;
+    this.weerdata['algemeneOmschrijving'] = JSON.stringify(data['weather'][0]['description']);
+    this.metString = JSON.stringify(data['weather'][0]['icon']).replace(/\"/g, "");
+    this.weerdata['weerIcon'] = "http://openweathermap.org/img/wn/" + this.metString + "@2x.png";
+    this.weerdata['weerBackground'] = JSON.stringify(data['weather'][0]['main']).replace(/\"/g, "");
+    if(this.weerdata['weerBackground'] == 'Haze' || this.weerdata['weerBackground'] == 'Smoke' || this.weerdata['weerBackground'] == 'Dust' || this.weerdata['weerBackground'] == 'Fog' || this.weerdata['weerBackground'] == 'Sand' || this.weerdata['weerBackground'] == 'Ash'){
+      this.weerdata['weerBackground'] = 'Mist';
+    }
+    this.weerdata['weerBackground'] = "assets/images/" + this.weerdata['weerBackground'].toLowerCase() + '.jpg';
   }
 
 }
